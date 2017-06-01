c682dc02d5af2b2b3ce957352b7bd247e4b0544b
Ritiek <ritiekmalhotra123@gmail.com>
2017-06-01 21:52:12 +0530

    Some fixes

--- gfeed.py
+++ gfeed.py
@@ -32,6 +32,8 @@ def PRReviewEvent(item):
 	created_at = item['payload']['comment']['created_at']
 	print created_at
 
+	print '{} reviewed pull request {} on {}'.format(user, number, repo)
+
 # open PR, close PR, comment on PR
 def PREvent(item):
 	#event = item['type']
@@ -53,7 +55,7 @@ def PREvent(item):
 	created_at = item['payload']['pull_request']['created_at']
 	print created_at
 
-	print '{} {}ed pull request {}#{}'.format(user, state, repo, number)
+	print '{} {} pull request {} on {}'.format(user, state, number, repo)
 	print title
 
 # comment on issue
@@ -61,27 +63,27 @@ def issueCommentEvent(item):
 	#event = item['type']
 	#print event
 	user = item['actor']['login']
-	print user
+	#print user
 	repo = item['repo']['name']
 
-	print repo
+	#print repo
 	link = item['payload']['issue']['html_url']
 	labels = item['payload']['issue']['labels']
 	for x in labels:
 		print x['name']
 	state = item['payload']['action']
-	print state
+	#print state
 	number = item['payload']['issue']['number']
-	print number
+	#print number
 	title = item['payload']['issue']['title']
-	print title
+	#print title
 	body = item['payload']['comment']['body']
-	print body
-	print link
+	#print body
+	#print link
 	created_at = item['payload']['comment']['created_at']
-	print created_at
+	#print created_at
 
-	print '{} commented on issue {}#{}'.format(user, repo, number)
+	print '{} commented on issue {} on {}'.format(user, number, repo)
 	print body
 
 # open issue, close issue
@@ -89,26 +91,26 @@ def issuesEvent(item):
 	#event = item['type']
 	#print event
 	user = item['actor']['login']
-	print user
+	#print user
 	repo = item['repo']['name']
-	print repo
+	#print repo
 	link = item['payload']['issue']['html_url']
 	title = item['payload']['issue']['title']
-	print title
+	#print title
 	state = item['payload']['action']
 	number = item['payload']['issue']['number']
-	print number
-	print state
+	#print number
+	#print state
 	try:
 		body = item['payload']['comment']['body']
-		print body
+		#print body
 	except:
 		pass
 	created_at = item['payload']['issue']['created_at']
-	print created_at
-	print link
+	#print created_at
+	#print link
 
-	print '{} {} issue {}#{}'.format(user, state, repo, number)
+	print '{} {} issue {} on {}'.format(user, state, number, repo)
 	print title
 
 # starred by following
@@ -116,25 +118,27 @@ def watchEvent(item):
 	#event = item['type']
 	#print event
 	user = item['actor']['login']
-	print user
+	#print user
 	repo = item['repo']['name']
-	print repo
+	#print repo
 	link = 'https://github.com/' + item['repo']['name']
-	print link
+	#print link
 	created_at = item['created_at']
-	print created_at
-	print '{} starred'
+	#print created_at
+	print '{} starred {}'.format(user, repo)
 
 # forked by following
 def forkEvent(item):
 	#event = item['type']
 	#print event
 	user = item['actor']['login']
-	print user
+	#print user
+	repo = item['repo']['name']
 	link = 'https://github.com/' + item['repo']['name']
-	print link
+	#print link
 	created_at = item['created_at']
-	print created_at
+	#print created_at
+	print '{} forked {}'.format(user, repo)
 
 # delete branch
 def deleteEvent(item):
@@ -151,6 +155,8 @@ def deleteEvent(item):
 	created_at = item['created_at']
 	print created_at
 
+	print '{} deleted branch {} at {}'.format(user, branch, repo)
+
 # push commits
 def pushEvent(item):
 	event = item['type']
@@ -166,7 +172,7 @@ def pushEvent(item):
 	created_at = item['created_at']
 	print created_at
 
-# create repo
+# create repo, branch
 def createEvent(item):
 	#event = item['type']
 	#print event
@@ -191,6 +197,8 @@ def publicEvent(item):
 	print link
 	created_at = item['created_at']
 	print created_at
+
+	print '{} made {} public'.format(user, repo)
 
 # add collab
 def memberEvent(item):
